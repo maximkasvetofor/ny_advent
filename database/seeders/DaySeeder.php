6619d2a81db0750720cf50c340e8d3b38e8788a8
maximkasvetofor <maximplay2018@gmail.com>
2024-08-02 17:46:17 +0400

    начальная стадия
    пог нали

---
+++ database/seeders/DaySeeder.php
@@ -0,0 +1,198 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DaySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('days')->insert(
+            [
+                'day' => '1',
+                'name' => 'Наименование рекомендации',
+                'description' => 'Описание рекомендации',
+                'link' => 'https://example.com',
+            ],
+            [
+                'day' => '2',
+                'name' => 'Наименование рекомендации',
+                'description' => 'Описание рекомендации',
+                'link' => 'https://example.com',
+            ],
+            [
+                'day' => '3',
+                'name' => 'Наименование рекомендации',
+                'description' => 'Описание рекомендации',
+                'link' => 'https://example.com',
+            ],
+            [
+                'day' => '4',
+                'name' => 'Наименование рекомендации',
+                'description' => 'Описание рекомендации',
+                'link' => 'https://example.com',
+            ],
+            [
+                'day' => '5',
+                'name' => 'Наименование рекомендации',
+                'description' => 'Описание рекомендации',
+                'link' => 'https://example.com',
+            ],
+            [
+                'day' => '6',
+                'name' => 'Наименование рекомендации',
+                'description' => 'Описание рекомендации',
+                'link' => 'https://example.com',
+            ],
+            [
+                'day' => '7',
+                'name' => 'Наименование рекомендации',
+                'description' => 'Описание рекомендации',
+                'link' => 'https://example.com',
+            ],
+            [
+                'day' => '8',
+                'name' => 'Наименование рекомендации',
+                'description' => 'Описание рекомендации',
+                'link' => 'https://example.com',
+            ],
+            [
+                'day' => '9',
+                'name' => 'Наименование рекомендации',
+                'description' => 'Описание рекомендации',
+                'link' => 'https://example.com',
+            ],
+            [
+                'day' => '10',
+                'name' => 'Наименование рекомендации',
+                'description' => 'Описание рекомендации',
+                'link' => 'https://example.com',
+            ],
+            [
+                'day' => '11',
+                'name' => 'Наименование рекомендации',
+                'description' => 'Описание рекомендации',
+                'link' => 'https://example.com',
+            ],
+            [
+                'day' => '12',
+                'name' => 'Наименование рекомендации',
+                'description' => 'Описание рекомендации',
+                'link' => 'https://example.com',
+            ],
+            [
+                'day' => '13',
+                'name' => 'Наименование рекомендации',
+                'description' => 'Описание рекомендации',
+                'link' => 'https://example.com',
+            ],
+            [
+                'day' => '14',
+                'name' => 'Наименование рекомендации',
+                'description' => 'Описание рекомендации',
+                'link' => 'https://example.com',
+            ],
+            [
+                'day' => '15',
+                'name' => 'Наименование рекомендации',
+                'description' => 'Описание рекомендации',
+                'link' => 'https://example.com',
+            ],
+            [
+                'day' => '16',
+                'name' => 'Наименование рекомендации',
+                'description' => 'Описание рекомендации',
+                'link' => 'https://example.com',
+            ],
+            [
+                'day' => '17',
+                'name' => 'Наименование рекомендации',
+                'description' => 'Описание рекомендации',
+                'link' => 'https://example.com',
+            ],
+            [
+                'day' => '18',
+                'name' => 'Наименование рекомендации',
+                'description' => 'Описание рекомендации',
+                'link' => 'https://example.com',
+            ],
+            [
+                'day' => '19',
+                'name' => 'Наименование рекомендации',
+                'description' => 'Описание рекомендации',
+                'link' => 'https://example.com',
+            ],
+            [
+                'day' => '20',
+                'name' => 'Наименование рекомендации',
+                'description' => 'Описание рекомендации',
+                'link' => 'https://example.com',
+            ],
+            [
+                'day' => '21',
+                'name' => 'Наименование рекомендации',
+                'description' => 'Описание рекомендации',
+                'link' => 'https://example.com',
+            ],
+            [
+                'day' => '22',
+                'name' => 'Наименование рекомендации',
+                'description' => 'Описание рекомендации',
+                'link' => 'https://example.com',
+            ],
+            [
+                'day' => '23',
+                'name' => 'Наименование рекомендации',
+                'description' => 'Описание рекомендации',
+                'link' => 'https://example.com',
+            ],
+            [
+                'day' => '24',
+                'name' => 'Наименование рекомендации',
+                'description' => 'Описание рекомендации',
+                'link' => 'https://example.com',
+            ],
+            [
+                'day' => '25',
+                'name' => 'Наименование рекомендации',
+                'description' => 'Описание рекомендации',
+                'link' => 'https://example.com',
+            ],
+            [
+                'day' => '26',
+                'name' => 'Наименование рекомендации',
+                'description' => 'Описание рекомендации',
+                'link' => 'https://example.com',
+            ],
+            [
+                'day' => '27',
+                'name' => 'Наименование рекомендации',
+                'description' => 'Описание рекомендации',
+                'link' => 'https://example.com',
+            ],
+            [
+                'day' => '28',
+                'name' => 'Наименование рекомендации',
+                'description' => 'Описание рекомендации',
+                'link' => 'https://example.com',
+            ],
+            [
+                'day' => '29',
+                'name' => 'Наименование рекомендации',
+                'description' => 'Описание рекомендации',
+                'link' => 'https://example.com',
+            ],
+            [
+                'day' => '31',
+                'name' => 'Наименование рекомендации',
+                'description' => 'Описание рекомендации',
+                'link' => 'https://example.com',
+            ],
+        );
+    }
+}
