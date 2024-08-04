954858d7fd6ee855efa599148d0e55d044f16c54
Bogdan <bogdan.aleksandrov.01@mail.ru>
2024-08-04 20:23:55 +0400

    УльтраМегапдейт

--- resources/views/admin/dayslist.blade.php
+++ resources/views/admin/dayslist.blade.php
@@ -35,7 +35,7 @@
                     <path d="M575.319 0.555176V9.39518H558.429V35.3952H547.509V9.39518H530.619V0.555176H575.319Z" fill="white"/>
                 </svg>
                 <div class="header-buttons">
-                    <button id="subscribe-btn">Вернуться</button>
+                    <button id="subscribe-btn"><a href="{{route("index")}}">Вернуться</a></button>
                     <button id="auth-btn">Выйти</button>
                 </div>
             </div>
@@ -63,7 +63,7 @@
                 Профиль
             </h3>
             <div class="row-item">
-                <img src="img/profile_pic.jpg" alt="Фотография профиля" class="wh-200">
+                <img src="{{asset("images/profile_pic.jpg")}}" alt="Фотография профиля" class="wh-200">
                 <form class="admin-form">
                     <!-- <p class="font-medium">Почта</p>
                     <span class="font-regular">kekkekchebutek12@mail.ru</span> -->
@@ -96,7 +96,8 @@
             <svg class="modal-close" width="40" height="40" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M32.9609 25L48.324 9.72222C50.5587 7.5 50.5587 4.16667 48.324 1.94444C47.486 0.833333 46.0894 0 44.6927 0C43.2961 0 41.8994 0.555555 40.7821 1.66667L25.1397 17.2222L9.77654 1.94444C7.5419 -0.277778 3.91061 -0.277778 1.95531 1.94444C0.837988 2.77778 0 4.16667 0 5.83333C0 7.5 0.558659 8.61111 1.67598 9.72222L17.0391 25L1.67598 40.2778C0.837989 41.3889 0 42.7778 0 44.4444C0 45.8333 0.558659 47.2222 1.67598 48.3333C2.7933 49.4444 4.18994 50 5.58659 50C6.98324 50 8.37989 49.4444 9.49721 48.3333L24.8603 33.0556L40.2235 48.3333C42.4581 50.5556 46.0894 50.5556 48.0447 48.3333C50.2793 46.1111 50.2793 42.5 48.0447 40.5556L32.9609 25Z" fill="white"/>
             </svg>
-            <div id="confirmation">
+            <div id="confirmation" class="font-medium">
+            </div>
                 <div class="modal-info">
                     <h2 class="modal-title" id="day-title">День № </h2>
                 </div>
@@ -119,7 +120,6 @@
                     </div>
                 </form>
                 <button id="submit" value="">Изменить</button>
-            </div>
         </div>
     </div>
 </main>
@@ -128,9 +128,9 @@
 <script>
     const ModalWrapper = document.getElementsByClassName("modal-wrapper")[0]
     const ModalDays = document.getElementsByClassName("modal-days")[0]
+    const CloseIcons = document.querySelectorAll(".modal-close")
     document.getElementsByClassName("form-days")[0].addEventListener("click",function(event){
         if(event.target.tagName === "LI"){
-            console.log("ura")
             var day = (event.target.getAttribute('value'));
             var request = new XMLHttpRequest(); // Создвём объект запроса
 
@@ -181,7 +181,17 @@
             }
         }
     })
-    function toggleModal(){
+    // Закрытие окна при нажатии на X
+    CloseIcons.forEach(icon => {
+        icon.addEventListener("click",toggle_modal)
+    });
+    // Закрытие окна при нажатии за пределы окна
+    ModalWrapper.addEventListener("click", function(){
+        if (event.target === ModalWrapper) {
+            toggle_modal();
+        }
+    })
+    function toggle_modal(){
         ModalWrapper.classList.add("inactive")
         ModalDays.classList.add("inactive")
     }
