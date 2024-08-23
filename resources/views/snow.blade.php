014044b970a1a0e3633fca01937a4b54ab65944c
Bogdan <bogdan.aleksandrov.01@mail.ru>
2024-08-24 03:06:13 +0400

    Снеежоооок

---
+++ resources/views/snow.blade.php
@@ -0,0 +1,109 @@
+<div class="snow">
+    <div class="snow__flake" style="font-size: 1.1em; animation-duration: 21s; animation-delay: 43s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1.2em; animation-duration: 24s; animation-delay: 38s;">﹡</div>
+    <div class="snow__flake" style="font-size: 0.9em; animation-duration: 22s; animation-delay: 44s;">﹡</div>
+    <div class="snow__flake" style="font-size: 0.8em; animation-duration: 28s; animation-delay: 6s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1em; animation-duration: 27s; animation-delay: 11s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1.1em; animation-duration: 25s; animation-delay: 24s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1.3em; animation-duration: 20s; animation-delay: 40s;">﹡</div>
+    <div class="snow__flake" style="font-size: 0.7em; animation-duration: 26s; animation-delay: 25s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1.2em; animation-duration: 23s; animation-delay: 30s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1.1em; animation-duration: 27s; animation-delay: 21s;">﹡</div>
+    <div class="snow__flake" style="font-size: 0.8em; animation-duration: 21s; animation-delay: 24s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1.5em; animation-duration: 30s; animation-delay: 29s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1.2em; animation-duration: 23s; animation-delay: 35s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1.2em; animation-duration: 26s; animation-delay: 26s;">﹡</div>
+    <div class="snow__flake" style="font-size: 0.8em; animation-duration: 26s; animation-delay: 15s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1.5em; animation-duration: 29s; animation-delay: 42s;">﹡</div>
+    <div class="snow__flake" style="font-size: 0.8em; animation-duration: 29s; animation-delay: 24s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1em; animation-duration: 28s; animation-delay: 31s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1.3em; animation-duration: 27s; animation-delay: 7s;">﹡</div>
+    <div class="snow__flake" style="font-size: 0.8em; animation-duration: 29s; animation-delay: 4s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1.1em; animation-duration: 27s; animation-delay: 6s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1.1em; animation-duration: 30s; animation-delay: 34s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1.3em; animation-duration: 21s; animation-delay: 8s;">﹡</div>
+    <div class="snow__flake" style="font-size: 0.9em; animation-duration: 27s; animation-delay: 22s;">﹡</div>
+    <div class="snow__flake" style="font-size: 0.9em; animation-duration: 29s; animation-delay: 40s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1.1em; animation-duration: 23s; animation-delay: 3s;">﹡</div>
+    <div class="snow__flake" style="font-size: 0.9em; animation-duration: 30s; animation-delay: 27s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1em; animation-duration: 29s; animation-delay: 36s;">﹡</div>
+    <div class="snow__flake" style="font-size: 0.9em; animation-duration: 24s; animation-delay: 16s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1.1em; animation-duration: 21s; animation-delay: 6s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1em; animation-duration: 25s; animation-delay: 39s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1.2em; animation-duration: 21s; animation-delay: 23s;">﹡</div>
+    <div class="snow__flake" style="font-size: 0.7em; animation-duration: 23s; animation-delay: 27s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1em; animation-duration: 29s; animation-delay: 12s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1.2em; animation-duration: 29s; animation-delay: 36s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1.2em; animation-duration: 24s; animation-delay: 7s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1.3em; animation-duration: 23s; animation-delay: 41s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1em; animation-duration: 28s; animation-delay: 32s;">﹡</div>
+    <div class="snow__flake" style="font-size: 0.9em; animation-duration: 24s; animation-delay: 10s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1.4em; animation-duration: 25s; animation-delay: 4s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1.3em; animation-duration: 23s; animation-delay: 28s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1em; animation-duration: 28s; animation-delay: 44s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1em; animation-duration: 30s; animation-delay: 38s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1.2em; animation-duration: 21s; animation-delay: 3s;">﹡</div>
+    <div class="snow__flake" style="font-size: 0.8em; animation-duration: 24s; animation-delay: 8s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1.3em; animation-duration: 26s; animation-delay: 1s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1.1em; animation-duration: 23s; animation-delay: 29s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1.1em; animation-duration: 22s; animation-delay: 41s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1.3em; animation-duration: 29s; animation-delay: 32s;">﹡</div>
+    <div class="snow__flake" style="font-size: 0.8em; animation-duration: 23s; animation-delay: 21s;">﹡</div>
+    <div class="snow__flake" style="font-size: 0.9em; animation-duration: 22s; animation-delay: 29s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1.3em; animation-duration: 26s; animation-delay: 3s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1.3em; animation-duration: 20s; animation-delay: 41s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1em; animation-duration: 22s; animation-delay: 0s;">﹡</div>
+    <div class="snow__flake" style="font-size: 0.9em; animation-duration: 28s; animation-delay: 26s;">﹡</div>
+    <div class="snow__flake" style="font-size: 1.2em; animation-duration: 23s; animation-delay: 28s;">﹡</div>
+    </div>
+<script>
+
+    const snow = document.querySelector('.snow')
+    const snowflakes = document.querySelectorAll('.snow__flake')
+
+    function getRndInteger(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) ) + min
+    }
+
+    function getRndFloat(min, max) {
+      return (Math.random() * (max - min) + min).toFixed(1)
+    }
+
+    snowflakes.forEach(snowflake => {
+      snowflake.style.fontSize = getRndFloat(1.5, 2) + 'em'
+      snowflake.style.animationDuration = getRndInteger(20, 30) + 's'
+      snowflake.style.animationDelay = getRndInteger(-1, snowflakes.length / 2) + 's'
+    })
+  
+</script>
+<style>
+    .snow {
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  z-index: 100;
+  display: flex;
+  justify-content: space-between;
+  pointer-events: none;
+}
+
+.snow__flake {
+  position: relative;
+  top: -1.5em;
+  color: #c1dcec;
+  animation-name: snowfall;
+  animation-timing-function: ease-in-out;
+  animation-iteration-count: infinite;
+  will-change: transform;
+}
+@keyframes snowfall {
+    0% {
+        transform: translateY(0);
+    }
+    100% {
+        transform: translateY(110vh);
+    }
+}
+</style>
