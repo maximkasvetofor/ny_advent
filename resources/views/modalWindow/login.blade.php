a34bcdc78d7ae1bcf4b78363838fdffc5c5f09a9
Dvati <104312550+Dvati@users.noreply.github.com>
2024-08-24 11:52:44 +0400

    ValidationMagikNation

--- resources/views/modalWindow/login.blade.php
+++ resources/views/modalWindow/login.blade.php
@@ -1,3 +1,49 @@
+<style>
+    i.ico {
+        display: inline-block;
+        background: red;
+        border-right: 50px;
+        width: 20px;
+        text-align: center;
+        font-style: normal;
+        font-weight: bold;
+    }
+    .errorBlock {
+        position: absolute;
+        top: 90%;
+        right: 20px;
+        transform: translateX(100%); /* initial transform: hidden to the right */
+        background: #ffe0e0;
+        border: 1px solid #a33a3a;
+        color: black;
+        padding: 10px;
+        border-radius: 10px;
+        visibility: hidden; /* initial visibility: hidden */
+        opacity: 0; /* initial opacity: 0 */
+        transition: transform 0.5s, opacity 0.5s, visibility 0s; /* add a transition effect to transform, opacity, and visibility */
+    }
+
+    .errorBlock.show {
+        visibility: visible; /* final visibility: visible */
+        opacity: 1; /* final opacity: 1 */
+        transform: translateX(0); /* final transform: slide in from the right */
+    }
+    .errorBlock .close {
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        cursor: pointer;
+    }
+    #errorBlockLogin {
+        transition: opacity 0.5s; /* add a transition effect to opacity */
+        opacity: 0; /* initial opacity is 0, so the block is hidden */
+    }
+
+    #errorBlockLogin.show {
+        opacity: 1; /* when the block is shown, opacity is 1 */
+    }
+</style>
+
 <div class="modal inactive modal-login">
     <svg class="modal-close" width="40" height="40" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M32.9609 25L48.324 9.72222C50.5587 7.5 50.5587 4.16667 48.324 1.94444C47.486 0.833333 46.0894 0 44.6927 0C43.2961 0 41.8994 0.555555 40.7821 1.66667L25.1397 17.2222L9.77654 1.94444C7.5419 -0.277778 3.91061 -0.277778 1.95531 1.94444C0.837988 2.77778 0 4.16667 0 5.83333C0 7.5 0.558659 8.61111 1.67598 9.72222L17.0391 25L1.67598 40.2778C0.837989 41.3889 0 42.7778 0 44.4444C0 45.8333 0.558659 47.2222 1.67598 48.3333C2.7933 49.4444 4.18994 50 5.58659 50C6.98324 50 8.37989 49.4444 9.49721 48.3333L24.8603 33.0556L40.2235 48.3333C42.4581 50.5556 46.0894 50.5556 48.0447 48.3333C50.2793 46.1111 50.2793 42.5 48.0447 40.5556L32.9609 25Z" fill="white"/>
@@ -6,30 +52,52 @@
         <h2 class="modal-title">Авторизация</h2>
         <h3 class="modal-subtitle">Вход</h3>
     </div>
-    <form class="modal__form" method="POST" action="{{route('advent.auth')}}">
+    <form class="modal__form" id="login-form" method="GET" action="{{route('advent.auth')}}">
         @csrf
         <div class="modal__form-item">
             <label for="email">Электронная Почта</label>
-            <input type="email" name="email" required="required">
-            @error('email')
-            <span class="error">{{ $message }}</span>
-            @enderror
+            <input type="text" name="email" required="required">
         </div>
 
         <div class="modal__form-item">
             <label for="password">Пароль</label>
             <input type="password" name="password" required="required">
-            @error('password')
-            <span class="error">{{ $message }}</span>
-            @enderror
         </div>
         <button type="submit">Войти</button>
-        @if ($errors->any())
-            <div class="error-block">
-                <p>Ошибка авторизации: {{ $errors->first() }}</p>
-            </div>
-        @endif
     </form>
 
     <button id="i_dont_have_account">У меня нет аккаунта</button>
+    <div class="errorBlock" id="errorBlockLogin">
+    </div>
 </div>
+
+<script>
+    const loginForm = document.getElementById('login-form');
+    const errorBlockLogin = document.getElementById('errorBlockLogin');
+    const csrfTokenLogin = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+    loginForm.addEventListener('submit', async (event) => {
+        event.preventDefault();
+
+        const formDataLogin = new FormData(loginForm);
+        const responseLogin = await fetch(loginForm.action, {
+            method: 'POST',
+            headers: {
+                'X-CSRF-TOKEN': csrfTokenLogin,
+            },
+            body: formDataLogin,
+        });
+        console.log(responseLogin);
+        if (!responseLogin.ok) {
+            const errorDataLogin = await responseLogin.json();
+            errorBlockLogin.innerHTML = `<i class="ico">&#9747;</i> Ошибка: <br>${errorDataLogin.error}` + `<i class="close" onclick="closeErrorLogin()">&#9747;</i>`;
+            errorBlockLogin.classList.add('show'); // add the show class to trigger the transition
+        } else {
+            window.location.href = '/';
+        }
+    });
+
+    function closeErrorLogin() {
+        errorBlockLogin.classList.remove('show');
+    }
+</script>
